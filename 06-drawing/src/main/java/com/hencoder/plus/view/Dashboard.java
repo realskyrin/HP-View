a91f336c62b7b4fb941c2d86fa1bfb40dfa9c0fe
skyrin <cnskyrin@163.com>
2018-10-23 08:03:56 +0800

    Initial commit

---
+++ 06-drawing/src/main/java/com/hencoder/plus/view/Dashboard.java
@@ -0,0 +1,91 @@
+package com.hencoder.plus.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PathDashPathEffect;
+import android.graphics.PathMeasure;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.hencoder.plus.Utils;
+
+public class Dashboard extends View {
+    private static final int ANGLE = 120;
+    private static final float RADIUS = Utils.dp2px(150);
+    private static final float LENGTH = Utils.dp2px(100);
+    Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    Path dash = new Path();
+    PathDashPathEffect effect;
+
+    public Dashboard(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    {
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(Utils.dp2px(2));
+        dash.addRect(0, 0, Utils.dp2px(2), Utils.dp2px(10), Path.Direction.CW);
+        Path arc = new Path();
+        arc.addArc(getWidth() / 2 - RADIUS,
+                getHeight() / 2 - RADIUS,
+                getWidth() / 2 + RADIUS ,
+                getHeight() / 2 + RADIUS,
+                90 + ANGLE / 2,
+                360 - ANGLE);
+        // 度量对象返回路径的长度，arc：被测量的路径 forceClosed：是否测量闭合路径长度
+        PathMeasure pathMeasure = new PathMeasure(arc, false);
+
+        // 通过使用指定的形状标记绘制的路径来对其进行划线。
+        // 这仅适用于绘画样式为STROKE或STROKE_AND_FILL时的绘图。
+        // 如果paint的样式为FILL，则忽略此效果。paint的strokeWidth不会影响结果。
+        effect = new PathDashPathEffect(
+                // 路径
+                dash,
+                // 每个形状之间的间距
+                (pathMeasure.getLength() - Utils.dp2px(2)) / 20,
+                // 在第一个形状之前的偏移量
+                0,
+                // 如何在标记每个位置时转换形状
+                PathDashPathEffect.Style.ROTATE);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        // 画线
+        canvas.drawArc(getWidth() / 2 - RADIUS,
+                getHeight() / 2 - RADIUS,
+                getWidth() / 2 + RADIUS ,
+                getHeight() / 2 + RADIUS,
+                90 + ANGLE / 2,
+                360 - ANGLE,
+                false, paint);
+
+        // 画刻度
+        paint.setPathEffect(effect);
+        canvas.drawArc(getWidth() / 2 - RADIUS,
+                getHeight() / 2 - RADIUS,
+                getWidth() / 2 + RADIUS ,
+                getHeight() / 2 + RADIUS,
+                90 + ANGLE / 2,
+                360 - ANGLE,
+                false, paint);
+        paint.setPathEffect(null);
+
+        // 画指针
+        canvas.drawLine(getWidth() / 2,
+                getHeight() / 2,
+                (float) Math.cos(Math.toRadians(getAngleFromMark(10))) * LENGTH + getWidth() / 2,
+                (float) Math.sin(Math.toRadians(getAngleFromMark(10))) * LENGTH + getHeight() / 2,
+                paint);
+
+    }
+
+    int getAngleFromMark(int mark) {
+        return (int) (90 + (float) ANGLE / 2 + (360 - (float) ANGLE) / 20 * mark);
+    }
+}
